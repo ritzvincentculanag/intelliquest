4195f37b9411113fb0cc8508a44b0c00de231fad
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-21 18:08:17 +0800

    update: move fragments to ui package

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/CreateFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/CreateFragment.kt
@@ -1,10 +1,11 @@
-package tech.ritzvincentculanag.intelliquest
+package tech.ritzvincentculanag.intelliquest.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import tech.ritzvincentculanag.intelliquest.R
 
 
 class CreateFragment : Fragment() {
