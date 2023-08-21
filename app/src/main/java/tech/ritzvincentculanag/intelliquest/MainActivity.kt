d051e9008aef3d1da90530185b79754988168ee2
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-21 18:24:21 +0800

    update: use AppCompatActivity

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/MainActivity.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/MainActivity.kt
@@ -1,8 +1,7 @@
 package tech.ritzvincentculanag.intelliquest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
