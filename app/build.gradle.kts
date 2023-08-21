0d18ab5129eb862a1d456d250506f8abe6245c19
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-21 17:31:06 +0800

    update: add navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,21 @@ android {
 }
 
 dependencies {
+    val room_version = "2.5.2"
+    val nav_version = "2.7.0"
+
+    // Room
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
