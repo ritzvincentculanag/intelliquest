66af5eabc3786b830d11347f598af1ab3b2218ef
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-23 22:33:46 +0800

    update: add test run for components and layers

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/LoginActivity.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/LoginActivity.kt
@@ -1,10 +1,16 @@
 package tech.ritzvincentculanag.intelliquest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import tech.ritzvincentculanag.intelliquest.databinding.ActivityLoginBinding
+import tech.ritzvincentculanag.intelliquest.factory.UserViewModelFactory
 import tech.ritzvincentculanag.intelliquest.viewmodel.LoginViewModel
+import tech.ritzvincentculanag.intelliquest.viewmodel.UserViewModel
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,6 +25,15 @@ class LoginActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         binding.viewModel = viewModel
+
+        val userViewModelFactory = UserViewModelFactory(application)
+        val userViewModel = ViewModelProvider(this, userViewModelFactory)[UserViewModel::class.java]
+
+        CoroutineScope(Dispatchers.IO).launch {
+            userViewModel.getUser("ritchiev", "J&Jwuth10").collect {
+                Log.d("COROTAG", it.toString())
+            }
+        }
     }
 
 }
