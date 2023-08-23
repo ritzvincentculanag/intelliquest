6e5243fd5e868da18f2f0c05f656a90548ed9263
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-23 23:56:33 +0800

    update: use validation to username and password and integrate ViewModel to View

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/LoginActivity.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/LoginActivity.kt
@@ -1,38 +1,66 @@
 package tech.ritzvincentculanag.intelliquest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.google.android.material.textfield.TextInputLayout
 import tech.ritzvincentculanag.intelliquest.databinding.ActivityLoginBinding
 import tech.ritzvincentculanag.intelliquest.factory.UserViewModelFactory
-import tech.ritzvincentculanag.intelliquest.viewmodel.LoginViewModel
+import tech.ritzvincentculanag.intelliquest.util.Snacks
+import tech.ritzvincentculanag.intelliquest.util.Validators
 import tech.ritzvincentculanag.intelliquest.viewmodel.UserViewModel
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var viewModel: LoginViewModel
+    private lateinit var viewModel: UserViewModel
+    private lateinit var factory: UserViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        factory = UserViewModelFactory(application)
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         binding.viewModel = viewModel
+        setContentView(binding.root)
+        setValidations()
+    }
 
-        val userViewModelFactory = UserViewModelFactory(application)
-        val userViewModel = ViewModelProvider(this, userViewModelFactory)[UserViewModel::class.java]
+    private fun setValidations() {
+        val containerUsername = binding.containerUsername
+        val containerPassword = binding.containerPassword
 
-        CoroutineScope(Dispatchers.IO).launch {
-            userViewModel.getUser("ritchiev", "J&Jwuth10").collect {
-                Log.d("COROTAG", it.toString())
+        binding.actionLogin.setOnClickListener {
+            val usernameIsValid = validateField(containerUsername)
+            val passwordIsValid = validateField(containerPassword)
+
+            if (!usernameIsValid && !passwordIsValid) {
+                Snacks.shortSnack(binding.root, "Fields are required")
+                return@setOnClickListener
             }
+        }
+        binding.inputUsername.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                validateField(containerUsername)
+            }
+        }
+        binding.inputPassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                validateField(containerPassword)
+            }
+        }
+    }
+
+    private fun validateField(field: TextInputLayout): Boolean {
+        val input = binding.inputUsername.text.toString()
+
+        return if (input.isEmpty()) {
+            Validators.setError(field)
+            false
+        } else {
+            Validators.clearErrors(field)
+            true
         }
     }
 
