0ee771c16bad5cb57809cee390ea9f78e3d0eea7
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 00:46:46 +0800

    update: add SessionManager to keep user logged in

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/util/SessionManager.kt
@@ -0,0 +1,35 @@
+package tech.ritzvincentculanag.intelliquest.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import tech.ritzvincentculanag.intelliquest.model.User
+
+private const val PREFERENCE_NAME = "Intelliquest"
+
+private const val USER_ACTIVE = "USER_ACTIVE"
+private const val USER_ID = "USER_ID"
+
+class SessionManager(context: Context) {
+
+    private val preferences: SharedPreferences
+    private val preferencesEditor: SharedPreferences.Editor
+
+    init {
+        preferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+        preferencesEditor = preferences.edit()
+    }
+
+    fun saveSession(user: User) {
+        preferencesEditor.putInt(USER_ID, user.userId)
+        preferencesEditor.putBoolean(USER_ACTIVE, true)
+        preferencesEditor.commit()
+    }
+
+    fun clearSession() {
+        preferencesEditor.clear()
+        preferencesEditor.commit()
+    }
+
+    fun userIsActive(): Boolean = preferences.getBoolean(USER_ACTIVE, false)
+
+}
