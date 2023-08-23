dd4ba8f685885e59e01cdcbddf8ffb21291c2709
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-23 22:33:17 +0800

    update: add initial UserRepository

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/repository/UserRepository.kt
@@ -0,0 +1,29 @@
+package tech.ritzvincentculanag.intelliquest.repository
+
+import kotlinx.coroutines.flow.Flow
+import tech.ritzvincentculanag.intelliquest.dao.UserDao
+import tech.ritzvincentculanag.intelliquest.model.User
+
+class UserRepository(private val userDao: UserDao) {
+
+    private val users: Flow<List<User>> = userDao.getUsers()
+
+    suspend fun insert(user: User) {
+        userDao.insert(user)
+    }
+
+    suspend fun update(user: User) {
+        userDao.update(user)
+    }
+
+    suspend fun delete(user: User) {
+        userDao.delete(user)
+    }
+
+    suspend fun getUser(username: String, password: String): User {
+        return userDao.getUser(username, password)
+    }
+
+    fun getUsers(): Flow<List<User>> = users
+
+}
