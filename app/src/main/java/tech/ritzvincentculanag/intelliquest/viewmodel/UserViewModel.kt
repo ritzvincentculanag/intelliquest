dfd3e87f51b4b9afb0c8cba4b1e246e8c6e38c8a
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 00:13:41 +0800

    update: add login method

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/viewmodel/UserViewModel.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/viewmodel/UserViewModel.kt
@@ -1,6 +1,7 @@
 package tech.ritzvincentculanag.intelliquest.viewmodel
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,8 +13,9 @@ import kotlinx.coroutines.launch
 import tech.ritzvincentculanag.intelliquest.db.AppDatabase
 import tech.ritzvincentculanag.intelliquest.model.User
 import tech.ritzvincentculanag.intelliquest.repository.UserRepository
+import tech.ritzvincentculanag.intelliquest.util.Snacks
 
-class UserViewModel(application: Application) : ViewModel() {
+class UserViewModel(private val application: Application) : ViewModel() {
 
     var inputUsername = MutableLiveData<String>()
     var inputPassword = MutableLiveData<String>()
@@ -41,13 +43,27 @@ class UserViewModel(application: Application) : ViewModel() {
 
     fun getUsers(): Flow<List<User>> = users
 
-    fun getUser(username: String, password: String): Flow<User> {
+    fun getUser(username: String, password: String): Flow<User?> {
         val userFlow = flow {
             val user = repository.getUser(username, password)
             emit(user)
         }
 
         return userFlow.flowOn(Dispatchers.IO)
+    }
+
+    suspend fun login(view: View) {
+        val username = inputUsername.value!!
+        val password = inputPassword.value!!
+
+        getUser(username, password).collect { user ->
+            if (user == null) {
+                Snacks.shortSnack(view, "User not found")
+                return@collect
+            }
+
+            Snacks.shortSnack(view, "Welcome user!")
+        }
     }
 
 }
