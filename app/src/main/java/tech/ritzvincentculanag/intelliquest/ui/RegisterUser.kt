af530dfd650fb8c73e9826624dea38e9f5fa2a8d
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 19:34:14 +0800

    update: add initial register user activity

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/RegisterUser.kt
@@ -0,0 +1,12 @@
+package tech.ritzvincentculanag.intelliquest.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.ritzvincentculanag.intelliquest.R
+
+class RegisterUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
