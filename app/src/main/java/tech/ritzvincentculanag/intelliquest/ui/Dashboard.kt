a7129d19e00872e322177773670d97dd78717028
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 23:01:31 +0800

    update: add layout for dashboard

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Dashboard.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Dashboard.kt
@@ -2,11 +2,9 @@ package tech.ritzvincentculanag.intelliquest.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import tech.ritzvincentculanag.intelliquest.R
 
 class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
     }
 }
