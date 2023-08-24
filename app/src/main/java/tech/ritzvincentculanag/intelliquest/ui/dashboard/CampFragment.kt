a07392c64b03ef4239bd4a84b3a94ed8e5ebd61e
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 22:41:30 +0800

    update: add initial fragments for navigation

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/CampFragment.kt
@@ -0,0 +1,21 @@
+package tech.ritzvincentculanag.intelliquest.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import tech.ritzvincentculanag.intelliquest.R
+
+
+class CampFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camp, container, false)
+    }
+
+}
