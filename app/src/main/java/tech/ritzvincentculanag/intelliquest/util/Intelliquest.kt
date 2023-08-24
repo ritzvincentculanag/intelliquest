e88341a28d325f7c6c2491af5c35efa409f72b5a
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 14:14:39 +0800

    update: apply dynamic colors to app

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/util/Intelliquest.kt
@@ -0,0 +1,12 @@
+package tech.ritzvincentculanag.intelliquest.util
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class Intelliquest : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
