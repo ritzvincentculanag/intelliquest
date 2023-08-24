2ef500f1ed2334595baa5b6b932f42b3d5c443ed
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 18:42:15 +0800

    update: enable bindings and set adapter to viewpager

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Introduction.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Introduction.kt
@@ -2,11 +2,23 @@ package tech.ritzvincentculanag.intelliquest.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import tech.ritzvincentculanag.intelliquest.R
+import tech.ritzvincentculanag.intelliquest.databinding.ActivityIntroductionBinding
+import tech.ritzvincentculanag.intelliquest.model.adapter.IntroductionAdapter
 
 class Introduction : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroductionBinding
+    private lateinit var adapter: IntroductionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_introduction)
+        binding = ActivityIntroductionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = IntroductionAdapter()
+        binding.viewPager.adapter = adapter
+        binding.viewPager.clipToPadding = false
+        binding.viewPager.clipChildren = false
+        binding.viewPagerIndicator.setViewPager(binding.viewPager)
     }
 }
