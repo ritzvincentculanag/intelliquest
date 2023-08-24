f05adbbddbc1cf2983dcfc5a543dc249891efc46
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-24 19:10:45 +0800

    update: check onCreate if user is logged in

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Introduction.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Introduction.kt
@@ -1,24 +1,37 @@
 package tech.ritzvincentculanag.intelliquest.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import tech.ritzvincentculanag.intelliquest.LoginActivity
 import tech.ritzvincentculanag.intelliquest.databinding.ActivityIntroductionBinding
 import tech.ritzvincentculanag.intelliquest.model.adapter.IntroductionAdapter
+import tech.ritzvincentculanag.intelliquest.util.SessionManager
 
 class Introduction : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroductionBinding
     private lateinit var adapter: IntroductionAdapter
+    private lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityIntroductionBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        binding = ActivityIntroductionBinding.inflate(layoutInflater)
         adapter = IntroductionAdapter()
+        sessionManager = SessionManager(applicationContext)
+
+        if (sessionManager.userIsActive()) {
+            val goToLogin = Intent(this, LoginActivity::class.java)
+            startActivity(goToLogin)
+            onDestroy()
+        }
+
         binding.viewPager.adapter = adapter
         binding.viewPager.clipToPadding = false
         binding.viewPager.clipChildren = false
         binding.viewPagerIndicator.setViewPager(binding.viewPager)
+
+        setContentView(binding.root)
     }
 }
