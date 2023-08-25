7ca1bd051de2637097c7257db416026ad931ae04
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-25 15:36:04 +0800

    update: implement bottom navigation

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Dashboard.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/Dashboard.kt
@@ -2,9 +2,26 @@ package tech.ritzvincentculanag.intelliquest.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import tech.ritzvincentculanag.intelliquest.databinding.ActivityDashboardBinding
 
 class Dashboard : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navigation = binding.navigationDashboard
+        val navHost = supportFragmentManager.findFragmentById(binding.navHostFragment.id)
+        val navController = navHost?.findNavController()
+
+        if (navController != null) {
+            NavigationUI.setupWithNavController(navigation, navController)
+        }
     }
 }
