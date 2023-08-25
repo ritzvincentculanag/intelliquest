9f45f1623483ce4d1cbee8f4d22fa70f757dbb88
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-26 00:07:22 +0800

    update: add loadListeners method

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/ProfileFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/ProfileFragment.kt
@@ -1,17 +1,21 @@
 package tech.ritzvincentculanag.intelliquest.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import tech.ritzvincentculanag.intelliquest.LoginActivity
 import tech.ritzvincentculanag.intelliquest.databinding.FragmentProfileBinding
 import tech.ritzvincentculanag.intelliquest.factory.UserViewModelFactory
 import tech.ritzvincentculanag.intelliquest.util.SessionManager
+import tech.ritzvincentculanag.intelliquest.util.Snacks
 import tech.ritzvincentculanag.intelliquest.viewmodel.UserViewModel
 
 class ProfileFragment : Fragment() {
@@ -25,14 +29,19 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        setUpFragment()
+        loadUserData()
+        loadListeners()
+
+        return binding.root
+    }
+
+    private fun setUpFragment() {
         factory = UserViewModelFactory(requireActivity().application)
         viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
         sessionManager = SessionManager(requireContext())
         binding = FragmentProfileBinding.inflate(layoutInflater)
-
-        loadUserData()
-
-        return binding.root
     }
 
     private fun loadUserData() {
@@ -41,6 +50,49 @@ class ProfileFragment : Fragment() {
             viewModel.getUser(userId).collect {
                 binding.user = it
             }
+        }
+    }
+
+    private fun loadListeners() {
+        binding.actionLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Logout")
+                .setMessage("Are you sure you want to logout?")
+                .setPositiveButton("Yes") { _, _ ->
+                    sessionManager.clearSession()
+                    startActivity(Intent(requireContext(), LoginActivity::class.java))
+                    onDestroy()
+                }
+                .setNegativeButton("No") { _, _ ->
+                    Snacks.shortSnack(
+                        view = binding.root,
+                        message = "Logout cancelled"
+                    )
+                }
+                .show()
+        }
+        binding.actionDeleteProfile.setOnClickListener {
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("Delete profile")
+                .setMessage("Are you sure you want to delete your profile? " +
+                        "All data associated with it will also be lost.")
+                .setPositiveButton("Yes") { _, _ ->
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val userId = sessionManager.getInt("USER_ID")
+                        viewModel.getUser(userId).collect {
+                            viewModel.delete(it!!)
+                            startActivity(Intent(requireContext(), LoginActivity::class.java))
+                            onDestroy()
+                        }
+                    }
+                }
+                .setNegativeButton("No") { _, _ ->
+                    Snacks.shortSnack(
+                        view = binding.root,
+                        message = "Delete cancelled"
+                    )
+                }
+                .show()
         }
     }
 
