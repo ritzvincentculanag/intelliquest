a4f75d97a758072263da094f543704589fb707dc
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-25 20:16:38 +0800

    update: add initial ProfileFragment implementation

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/ProfileFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/ProfileFragment.kt
@@ -5,16 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import tech.ritzvincentculanag.intelliquest.R
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import tech.ritzvincentculanag.intelliquest.databinding.FragmentProfileBinding
+import tech.ritzvincentculanag.intelliquest.factory.UserViewModelFactory
+import tech.ritzvincentculanag.intelliquest.util.SessionManager
+import tech.ritzvincentculanag.intelliquest.viewmodel.UserViewModel
 
 class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+    private lateinit var viewModel: UserViewModel
+    private lateinit var factory: UserViewModelFactory
+    private lateinit var sessionManager: SessionManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        factory = UserViewModelFactory(requireActivity().application)
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+        sessionManager = SessionManager(requireContext())
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        loadUserData()
+
+        return binding.root
+    }
+
+    private fun loadUserData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val userId = sessionManager.getInt("USER_ID")
+            viewModel.getUser(userId).collect {
+                binding.user = it
+            }
+        }
     }
 
 }
