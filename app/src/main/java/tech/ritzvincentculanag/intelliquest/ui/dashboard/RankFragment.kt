a9c2a9448ee9b50f793c6b49f9011cda9430d75b
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-26 00:41:59 +0800

    update: add logic for RankFragment

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/RankFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/RankFragment.kt
@@ -5,16 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import tech.ritzvincentculanag.intelliquest.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import tech.ritzvincentculanag.intelliquest.databinding.FragmentRankBinding
+import tech.ritzvincentculanag.intelliquest.factory.UserViewModelFactory
+import tech.ritzvincentculanag.intelliquest.model.adapter.RankAdapter
+import tech.ritzvincentculanag.intelliquest.viewmodel.UserViewModel
 
 class RankFragment : Fragment() {
+
+    private lateinit var binding: FragmentRankBinding
+    private lateinit var viewModel: UserViewModel
+    private lateinit var factory: UserViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false)
+    ): View {
+
+        setupFragment()
+        setupRecyclerView()
+
+        return binding.root
+    }
+
+    private fun setupFragment() {
+        factory = UserViewModelFactory(requireActivity().application)
+        viewModel = ViewModelProvider(this, factory)[UserViewModel::class.java]
+        binding = FragmentRankBinding.inflate(layoutInflater)
+    }
+
+    private fun setupRecyclerView() {
+        val users = viewModel.getUsers()
+        val adapter = RankAdapter()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            users.collect { users ->
+                adapter.setUsers(users)
+            }
+        }
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
