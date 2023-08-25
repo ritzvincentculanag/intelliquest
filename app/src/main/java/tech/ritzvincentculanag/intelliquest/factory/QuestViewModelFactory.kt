d9eda11c1519b8f34f9a9c9bd9505bfe4c21e758
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-26 02:30:20 +0800

    update: add quest view model factory

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/factory/QuestViewModelFactory.kt
@@ -0,0 +1,18 @@
+package tech.ritzvincentculanag.intelliquest.factory
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import tech.ritzvincentculanag.intelliquest.viewmodel.QuestViewModel
+
+class QuestViewModelFactory(private val application: Application): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(QuestViewModel::class.java)) {
+            QuestViewModel(application) as T
+        } else {
+            throw IllegalArgumentException("ViewModel not found")
+        }
+    }
+
+}
