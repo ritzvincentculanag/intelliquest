bdbf116a53cec1b6ad5ad7462d760e9e9259e35a
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-26 02:51:43 +0800

    update: add initial implementation of QuestFragment

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/QuestFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/QuestFragment.kt
@@ -5,16 +5,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import tech.ritzvincentculanag.intelliquest.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import tech.ritzvincentculanag.intelliquest.databinding.FragmentQuestBinding
+import tech.ritzvincentculanag.intelliquest.factory.QuestViewModelFactory
+import tech.ritzvincentculanag.intelliquest.model.adapter.QuestAdapter
+import tech.ritzvincentculanag.intelliquest.util.SessionManager
+import tech.ritzvincentculanag.intelliquest.viewmodel.QuestViewModel
 
 class QuestFragment : Fragment() {
+
+    private lateinit var binding: FragmentQuestBinding
+    private lateinit var viewModel: QuestViewModel
+    private lateinit var factory: QuestViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quest, container, false)
+    ): View {
+
+        setupFragment()
+        setupRecyclerView()
+
+        return binding.root
+    }
+
+    private fun setupFragment() {
+        factory = QuestViewModelFactory(requireActivity().application)
+        viewModel = ViewModelProvider(this, factory)[QuestViewModel::class.java]
+        binding = FragmentQuestBinding.inflate(layoutInflater)
+    }
+
+    private fun setupRecyclerView() {
+        val adapter = QuestAdapter()
+        val session = SessionManager(requireContext())
+        val userId = session.getInt("USER_ID")
+
+        CoroutineScope(Dispatchers.Default).launch {
+            viewModel.getUserQuests(userId).collect {
+                adapter.setQuests(it)
+            }
+        }
+        binding.questList.adapter = adapter
+        binding.questList.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
