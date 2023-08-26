a5fef78448cf61ab7773468c9f33ab254506ede2
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-26 21:17:52 +0800

    update: add RegisterUserViewModelFactory

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/factory/RegisterUserViewModelFactory.kt
@@ -0,0 +1,18 @@
+package tech.ritzvincentculanag.intelliquest.factory
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import tech.ritzvincentculanag.intelliquest.viewmodel.RegisterUserViewModel
+
+class RegisterUserViewModelFactory(private val application: Application): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(RegisterUserViewModel::class.java)) {
+            RegisterUserViewModel(application) as T
+        } else {
+            throw IllegalArgumentException("ViewModel not found")
+        }
+    }
+
+}
