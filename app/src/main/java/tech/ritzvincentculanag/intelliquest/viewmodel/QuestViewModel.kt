2412d4de08ea5f2f18a48c22c43de65a697bea96
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-27 14:25:18 +0800

    update: implement getUserQuests

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/viewmodel/QuestViewModel.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/viewmodel/QuestViewModel.kt
@@ -8,21 +8,43 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import tech.ritzvincentculanag.intelliquest.db.AppDatabase
 import tech.ritzvincentculanag.intelliquest.model.Quest
+import tech.ritzvincentculanag.intelliquest.model.relationship.UserQuests
 import tech.ritzvincentculanag.intelliquest.repository.QuestRepository
+import tech.ritzvincentculanag.intelliquest.repository.UserRepository
+import tech.ritzvincentculanag.intelliquest.util.SessionManager
 
-class QuestViewModel(application: Application) : ViewModel() {
+class QuestViewModel(private val application: Application) : ViewModel() {
 
     private val repository: QuestRepository = QuestRepository(AppDatabase.getDatabase(application).questDao())
-    private val userQuests: Flow<List<Quest>> = repository.getQuests()
+    private val userRepository: UserRepository = UserRepository(AppDatabase.getDatabase(application).userDao())
+    private val quests: Flow<List<Quest>> = repository.getQuests()
+    private val userQuests: Flow<List<UserQuests>> = userRepository.getUserQuests()
 
     fun getQuests(): Flow<List<Quest>> {
-        val userQuestsFlow = flow {
-            userQuests.collect {
+        val questsFlow = flow {
+            quests.collect {
                 emit(it)
             }
         }
 
-        return userQuestsFlow.flowOn(Dispatchers.Main)
+        return questsFlow.flowOn(Dispatchers.Main)
+    }
+
+    fun getUserQuests(): Flow<List<Quest>> {
+        val userQuestsFlow = flow {
+            val session = SessionManager(application)
+            val userId = session.getInt("USER_ID")
+
+            userQuests.collect { items ->
+                items.forEach {
+                    if (it.user.userId == userId) {
+                        emit(it.quests)
+                    }
+                }
+            }
+        }
+
+        return userQuestsFlow.flowOn(Dispatchers.Default)
     }
 
 }
