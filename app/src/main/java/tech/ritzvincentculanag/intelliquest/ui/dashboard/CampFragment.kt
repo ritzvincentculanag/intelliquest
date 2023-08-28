b2fc3ae81550513a1a95288208be96f92ce0bd00
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 12:11:50 +0800

    update: implement QuestAdapterEvent

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/CampFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/CampFragment.kt
@@ -12,17 +12,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import tech.ritzvincentculanag.intelliquest.databinding.FragmentCampBinding
+import tech.ritzvincentculanag.intelliquest.model.Quest
 import tech.ritzvincentculanag.intelliquest.model.QuestState
 import tech.ritzvincentculanag.intelliquest.model.adapter.QuestCampAdapter
 import tech.ritzvincentculanag.intelliquest.viewmodel.QuestViewModel
 import tech.ritzvincentculanag.intelliquest.viewmodel.factory.QuestViewModelFactory
 
 
-class CampFragment : Fragment() {
+class CampFragment : Fragment(), QuestCampAdapter.QuestAdapterEvent {
 
     private lateinit var binding: FragmentCampBinding
     private lateinit var viewModel: QuestViewModel
     private lateinit var factory: QuestViewModelFactory
+    private var quests = mutableListOf<Quest>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +38,15 @@ class CampFragment : Fragment() {
         return binding.root
     }
 
+    override fun onItemClick(position: Int) {
+        val quest = quests[position]
+        val directions = CampFragmentDirections.actionCampFragmentToCreateQuest(
+            questState = QuestState(isCreating = false),
+            quest = quest
+        )
+        findNavController().navigate(directions)
+    }
+
     private fun setupFragment() {
         factory = QuestViewModelFactory(requireActivity().application)
         viewModel = ViewModelProvider(this, factory)[QuestViewModel::class.java]
@@ -43,14 +54,17 @@ class CampFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        val adapter = QuestCampAdapter()
+        val adapter = QuestCampAdapter(this)
         binding.questList.adapter = adapter
         binding.questList.layoutManager = LinearLayoutManager(requireContext())
 
         CoroutineScope(Dispatchers.Default).launch {
             viewModel.getUserQuests().collect {
+                quests.clear()
+                quests.addAll(it)
+
                 activity?.runOnUiThread {
-                    adapter.setQuests(it)
+                    adapter.setQuests(quests)
                 }
             }
         }
