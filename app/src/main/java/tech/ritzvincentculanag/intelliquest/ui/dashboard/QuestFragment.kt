f55dc8e2c96edd907eca47a05adcad3c82d36723
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 19:22:08 +0800

    update: implement QuestInterface

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/QuestFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/dashboard/QuestFragment.kt
@@ -6,20 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import tech.ritzvincentculanag.intelliquest.databinding.FragmentQuestBinding
+import tech.ritzvincentculanag.intelliquest.model.Quest
 import tech.ritzvincentculanag.intelliquest.model.adapter.QuestAdapter
 import tech.ritzvincentculanag.intelliquest.viewmodel.QuestViewModel
 import tech.ritzvincentculanag.intelliquest.viewmodel.factory.QuestViewModelFactory
 
-class QuestFragment : Fragment() {
+class QuestFragment : Fragment(), QuestAdapter.QuestInterface {
 
     private lateinit var binding: FragmentQuestBinding
     private lateinit var viewModel: QuestViewModel
     private lateinit var factory: QuestViewModelFactory
+
+    private val quests = mutableListOf<Quest>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +36,12 @@ class QuestFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStartClick(position: Int) {
+        val quest = quests[position]
+        val direction = QuestFragmentDirections.actionQuestFragmentToQuestActivity(quest)
+        findNavController().navigate(direction)
+    }
+
     private fun setupFragment() {
         factory = QuestViewModelFactory(requireActivity().application)
         viewModel = ViewModelProvider(this, factory)[QuestViewModel::class.java]
@@ -39,7 +49,7 @@ class QuestFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        val adapter = QuestAdapter()
+        val adapter = QuestAdapter(this)
 
         binding.questList.adapter = adapter
         binding.questList.layoutManager = LinearLayoutManager(requireContext())
@@ -47,8 +57,9 @@ class QuestFragment : Fragment() {
         CoroutineScope(Dispatchers.Default).launch {
             viewModel.getQuests().collect {
                 activity?.runOnUiThread {
-                    adapter.setQuests(it)
-                    showNoQuests(adapter)
+                    quests.clear()
+                    quests.addAll(it)
+                    adapter.setQuests(quests)
                 }
             }
         }
