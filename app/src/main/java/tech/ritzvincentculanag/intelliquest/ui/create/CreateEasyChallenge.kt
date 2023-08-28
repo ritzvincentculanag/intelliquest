51b3a807c302b26d713c231ae08468e617314489
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 13:24:00 +0800

    update: add initial fragments for creating challenges

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/create/CreateEasyChallenge.kt
@@ -0,0 +1,19 @@
+package tech.ritzvincentculanag.intelliquest.ui.create
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import tech.ritzvincentculanag.intelliquest.R
+
+class CreateEasyChallenge : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_easy_challenge, container, false)
+    }
+}
