a2401baddf73885108410869b62a8056b7ec3831
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 13:07:00 +0800

    update: add constants for scores

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/model/QuestType.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/model/QuestType.kt
@@ -1,5 +1,9 @@
 package tech.ritzvincentculanag.intelliquest.model
 
+const val SCORE_EASY = 4
+const val SCORE_MEDIUM = 7
+const val SCORE_HARD = 9
+
 enum class QuestType {
     EASY,
     MEDIUM,
