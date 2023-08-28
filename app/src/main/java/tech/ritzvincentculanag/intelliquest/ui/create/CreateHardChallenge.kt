5e58d7b62fcc0a2354533628486349240d97765d
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 18:18:16 +0800

    update: add initial creation of hard challenge

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/create/CreateHardChallenge.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/create/CreateHardChallenge.kt
@@ -5,15 +5,65 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import tech.ritzvincentculanag.intelliquest.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import tech.ritzvincentculanag.intelliquest.databinding.FragmentCreateHardChallengeBinding
+import tech.ritzvincentculanag.intelliquest.db.AppDatabase
+import tech.ritzvincentculanag.intelliquest.model.Challenge
+import tech.ritzvincentculanag.intelliquest.model.Quest
+import tech.ritzvincentculanag.intelliquest.repository.ChallengeRepository
+import tech.ritzvincentculanag.intelliquest.util.Snacks
 
-class CreateHardChallenge : BottomSheetDialogFragment() {
+class CreateHardChallenge(private val quest: Quest) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentCreateHardChallengeBinding
+    private lateinit var repository: ChallengeRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_hard_challenge, container, false)
+    ): View {
+        setupFragment()
+        setupOnClickListeners()
+
+        return binding.root
     }
+
+    private fun setupFragment() {
+        binding = FragmentCreateHardChallengeBinding.inflate(layoutInflater)
+        repository = ChallengeRepository(AppDatabase.getDatabase(requireContext()).challengeDao())
+    }
+
+    private fun setupOnClickListeners() {
+        binding.actionSaveHard.setOnClickListener {
+            createChallenge()
+        }
+    }
+
+    private fun createChallenge() {
+        val questionIsValid = binding.inputHardQuestion.text.toString().isNotEmpty()
+        val answerIsValid = binding.inputHardAnswer.text.toString().isNotEmpty()
+
+        if (!questionIsValid && !answerIsValid) {
+            Snacks.longSnack(
+                view = binding.root,
+                message = "Some fields have invalid values"
+            )
+        }
+
+        val challenge = Challenge(
+            challengeId = 0,
+            originQuestId = quest.questId,
+            question = binding.inputHardQuestion.text.toString(),
+            answer = binding.inputHardAnswer.text.toString()
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            repository.insert(challenge)
+        }
+
+        dismiss()
+    }
+
 }
