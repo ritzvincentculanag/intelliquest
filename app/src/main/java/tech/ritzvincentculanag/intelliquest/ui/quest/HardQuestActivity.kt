3073fe5e2cab43c6337d6a8e0f33e83dc37da58e
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 21:27:10 +0800

    update: add initial activities for other quest types

---
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/quest/HardQuestActivity.kt
@@ -0,0 +1,12 @@
+package tech.ritzvincentculanag.intelliquest.ui.quest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.ritzvincentculanag.intelliquest.R
+
+class HardQuestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hard_quest)
+    }
+}
