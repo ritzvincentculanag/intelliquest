d8c1fe354fe60d81968e0c878ed700a9eec7f1cd
ritzvincentculanag <rexcellent.rawr@gmail.com>
2023-08-28 14:36:15 +0800

    update: setup action for creating challenges

--- app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/create/ChallengesFragment.kt
+++ app/src/main/java/tech/ritzvincentculanag/intelliquest/ui/create/ChallengesFragment.kt
@@ -5,16 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import tech.ritzvincentculanag.intelliquest.R
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import tech.ritzvincentculanag.intelliquest.databinding.FragmentChallengesBinding
+import tech.ritzvincentculanag.intelliquest.model.QuestType
 
 class ChallengesFragment : Fragment() {
+
+    private val args by navArgs<ChallengesFragmentArgs>()
+
+    private lateinit var binding: FragmentChallengesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_challenges, container, false)
+    ): View {
+        setupFragment()
+        setupOnClickListeners()
+
+        return binding.root
+    }
+
+    private fun setupFragment() {
+        binding = FragmentChallengesBinding.inflate(layoutInflater)
+    }
+
+    private fun setupOnClickListeners() {
+        binding.actionCreateChallenge.setOnClickListener {
+            val questType = args.quest.questType
+            val createFragment: BottomSheetDialogFragment = when (questType) {
+                QuestType.EASY -> CreateEasyChallenge()
+                QuestType.MEDIUM -> CreateMediumChallenge()
+                QuestType.HARD -> CreateHardChallenge()
+                else -> {
+                    throw IllegalArgumentException("Invalid quest type")
+                }
+            }
+
+            createFragment.show(activity?.supportFragmentManager!!, "CREATE_QUEST")
+        }
     }
 
 }
